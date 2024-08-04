5519cced126266a107c18b5b1229ec103a9dd0c3
Sebastian <tatan873@gmail.com>
2024-08-04 00:04:49 -0500

    add inheranced

--- project-inventory/src/main/java/sp/electronics/core/models/ProductoEntity.java
+++ project-inventory/src/main/java/sp/electronics/core/models/ProductoEntity.java
@@ -1,6 +1,5 @@
 package sp.electronics.core.models;
 
-import java.sql.Date;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,26 +11,23 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 
 @Entity
-@Table(name = "Contacto")
-public class ContactoEntity {
+@Table(name = "Producto" )
+public class ProductoEntity {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer conId;
-    private Integer conTipoDocumento;
-    private Integer conNumDocumento;
-    private String conNombre;
-    private String conApellido;
-    private Date conFechaCrea;
-    private String conDireccion;
-    private Integer conTel;
-    private String conEmail;
+    private Integer proCodigo;
 
+    private Integer proImagen; //TOO
+    private String proModelo;
+    private String proDescripcion;
+    private Integer proCantidad;
+    private Double proPrecioUnitario;
+    private Integer proEstado;
 }
